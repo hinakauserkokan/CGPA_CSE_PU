e605102112960cd2bccd16c38c87dacce176b8ff
hinakauserkokan <43753966+hinakauserkokan@users.noreply.github.com>
2020-02-11 23:56:36 +0530

    Update GPA_7.java

--- app/src/main/java/com/example/cgpa/GPA_7.java
+++ app/src/main/java/com/example/cgpa/GPA_7.java
@@ -44,22 +44,22 @@ public class GPA_7 extends AppCompatActivity {
                     case 0:
                         a = 10;
                         break;
-                    case 2:
+                    case 1:
                         a = 9;
                         break;
-                    case 3:
+                    case 2:
                         a = 8;
                         break;
-                    case 4:
+                    case 3:
                         a= 7;
                         break;
-                    case 5:
+                    case 4:
                         a = 6;
                         break;
-                    case 6:
+                    case 5:
                         a = 5;
                         break;
-                    case 7:
+                    case 6:
                         a =0;
                         break;
                 }
@@ -77,22 +77,22 @@ public class GPA_7 extends AppCompatActivity {
                     case 0:
                         b = 10;
                         break;
-                    case 2:
+                    case 1:
                         b = 9;
                         break;
-                    case 3:
+                    case 2:
                         b = 8;
                         break;
-                    case 4:
+                    case 3:
                         b= 7;
                         break;
-                    case 5:
+                    case 4:
                         b = 6;
                         break;
-                    case 6:
+                    case 5:
                         b = 5;
                         break;
-                    case 7:
+                    case 6:
                         b =0;
                         break;
                 }
@@ -111,22 +111,22 @@ public class GPA_7 extends AppCompatActivity {
                     case 0:
                         c = 10;
                         break;
-                    case 2:
+                    case 1:
                         c = 9;
                         break;
-                    case 3:
+                    case 2:
                         c = 8;
                         break;
-                    case 4:
+                    case 3:
                         c = 7;
                         break;
-                    case 5:
+                    case 4:
                         c = 6;
                         break;
-                    case 6:
+                    case 5:
                         c = 5;
                         break;
-                    case 7:
+                    case 6:
                         c = 0;
                         break;
                 }
@@ -144,22 +144,22 @@ public class GPA_7 extends AppCompatActivity {
                     case 0:
                         d = 10;
                         break;
-                    case 2:
+                    case 1:
                         d = 9;
                         break;
-                    case 3:
+                    case 2:
                         d = 8;
                         break;
-                    case 4:
+                    case 3:
                         d = 7;
                         break;
-                    case 5:
+                    case 4:
                         d = 6;
                         break;
-                    case 6:
+                    case 5:
                         d = 5;
                         break;
-                    case 7:
+                    case 6:
                         d = 0;
                         break;
                 }
@@ -292,6 +292,139 @@ public class GPA_7 extends AppCompatActivity {
                         s = 5;
                         break;
                     case 7:
+                        s = 0;
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        spinp1.setAdapter(arrayAdapter);
+        spinp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        p = 10;
+                        break;
+                    case 1:
+                        p = 9;
+                        break;
+                    case 2:
+                        p = 8;
+                        break;
+                    case 3:
+                        p = 7;
+                        break;
+                    case 4:
+                        p = 6;
+                        break;
+                    case 5:
+                        p = 5;
+                        break;
+                    case 6:
+                        p = 0;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinp2.setAdapter(arrayAdapter);
+        spinp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        q = 10;
+                        break;
+                    case 1:
+                        q = 9;
+                        break;
+                    case 2:
+                        q = 8;
+                        break;
+                    case 3:
+                        q = 7;
+                        break;
+                    case 4:
+                        q = 6;
+                        break;
+                    case 5:
+                        q = 5;
+                        break;
+                    case 6:
+                        q = 0;
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinp3.setAdapter(arrayAdapter);
+        spinp3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        r = 10;
+                        break;
+                    case 1:
+                        r = 9;
+                        break;
+                    case 2:
+                        r = 8;
+                        break;
+                    case 3:
+                        r = 7;
+                        break;
+                    case 4:
+                        r = 6;
+                        break;
+                    case 5:
+                        r = 5;
+                        break;
+                    case 6:
+                        r = 0;
+                        break;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        spinp4.setAdapter(arrayAdapter);
+        spinp4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0:
+                        s = 10;
+                        break;
+                    case 1:
+                        s = 9;
+                        break;
+                    case 2:
+                        s = 8;
+                        break;
+                    case 3:
+                        s = 7;
+                        break;
+                    case 4:
+                        s = 6;
+                        break;
+                    case 5:
+                        s = 5;
+                        break;
+                    case 6:
                         s = 0;
                         break;
                 }
